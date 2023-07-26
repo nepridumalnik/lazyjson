8cc96d206bf1b7bca86882f4fb1bc321d479418e
Dudnik Pavel <Pavel.Dudnik@infotecs.ru>
2023-07-26 13:29:02 +0700

    Добавлена заготовка JSON объекта

---
+++ src/lazyjson/json.hpp
@@ -0,0 +1,34 @@
+#pragma once
+
+#include <variant>
+#include <string>
+#include <unordered_map>
+#include <string_view>
+
+namespace lazyjson
+{
+
+/// @brief Тип значения
+using element = std::variant<int, bool, std::string>;
+
+/// @brief JSON объект
+class Json
+{
+public:
+    element& operator[](const std::string& key)
+    {
+        auto& it = obj_.find(key);
+        if(it != obj_.end())
+        {
+            return it->second;
+        }
+
+        return obj_[key];
+    }
+
+private:
+    /// @brief Список в виде ключ/значение
+    std::unordered_map<std::string, element> obj_;
+};
+
+} // namespace lazyjson
--- src/tests/test.cpp
+++ src/tests/test.cpp
@@ -1,7 +1,15 @@
 #include <gtest/gtest.h>
 
+#include <lazyjson/json.hpp>
+
+namespace json = lazyjson;
+
 TEST(Test, MapTesting)
 {
-    ASSERT_TRUE(true);
-    ASSERT_FALSE(false);
+    json::Json json;
+    json::element& it = json["test"];
+    it = true;
+
+    bool res = std::get<bool>(it);
+    ASSERT_THROW(int resInt = std::get<int>(it), std::bad_variant_access);
 }
