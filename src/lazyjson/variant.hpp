78313a42496caeb8ee6f5685bd5823e9423108d1
Dudnik Pavel <ne.pridumal.nik@yandex.ru>
2023-07-27 20:33:45 +0700

    Добавлен класс variant

---
+++ src/lazyjson/variant.hpp
@@ -0,0 +1,121 @@
+#pragma once
+
+#include <typeindex>
+#include <type_traits>
+#include <stdexcept>
+#include <algorithm>
+#include <cstddef>
+
+namespace lazyjson
+{
+
+/// @brief Хранилище произвольных типов
+/// @tparam ...Ts
+template<typename... Ts>
+class variant
+{
+public:
+    /// @brief Конструктор по умолчанию
+    variant()
+    {
+    }
+
+    /// @brief Конструктор с присваиванием значения
+    /// @tparam T Тип значения
+    /// @param value Значение
+    template<typename T>
+    explicit variant(const T& value)
+    {
+        operator=(value);
+    }
+
+    /// @brief Деструктор
+    ~variant()
+    {
+        clear();
+    }
+
+    /// @brief Является ли хранимое значение искомого типа
+    /// @tparam T искомый тип
+    /// @return True, если является
+    template<typename T>
+    bool is_type() const
+    {
+        return m_type == typeid(T);
+    }
+
+    /// @brief Получить значение требуемого типа
+    /// @tparam T тип значения
+    /// @return Значение
+    template<typename T>
+    T get() const
+    {
+        if(!is_type<T>())
+        {
+            throw std::bad_variant_access();
+        }
+
+        return *reinterpret_cast<const T*>(m_data);
+    }
+
+    /// @brief Оператор присваивания для значения требуемого типа
+    /// @tparam T тип значения
+    /// @return Ссылка на экземпляр
+    template<typename T>
+    variant& operator=(const T& value)
+    {
+        if(!is_valid_type<T>())
+        {
+            throw std::bad_variant_access();
+        }
+
+        constexpr std::size_t offset = (m_alignment - (sizeof(T) % m_alignment)) % m_alignment;
+        std::byte* aligned_data = m_data + offset;
+
+        if(m_type != typeid(void))
+        {
+            reinterpret_cast<const T*>(m_data)->~T();
+        }
+
+        new(aligned_data) T(value);
+        m_type = typeid(T);
+
+        return *this;
+    }
+
+    /// @brief Очистка содержимого
+    void clear()
+    {
+        if(m_type != typeid(void))
+        {
+            void* data_ptr = static_cast<void*>(m_data);
+            std::destroy_at(static_cast<typename std::aligned_union<0, Ts...>::type*>(data_ptr));
+            std::memset(data_ptr, 0, m_max_size);
+            m_type = typeid(void);
+        }
+    }
+
+    /// @brief Проверка допустим ли тип
+    /// @tparam T тип
+    /// @return True, если да
+    template<typename T>
+    bool is_valid_type() const
+    {
+        return ((std::is_same_v<T, Ts> || ...));
+    }
+
+private:
+    /// @brief Размер хранилища
+    static constexpr size_t m_max_size = sizeof(std::aligned_union_t<0, Ts...>);
+
+    /// @brief Размер выравнивания
+    static constexpr size_t m_alignment = alignof(std::aligned_union_t<0, Ts...>);
+
+    /// @brief Хранимый тип
+    std::type_index m_type = typeid(void);
+
+    /// @brief Хранилище
+    std::byte m_data[m_max_size];
+};
+
+} // namespace lazyjson
---
+++ src/tests/variant.cpp
@@ -0,0 +1,97 @@
+#include <gtest/gtest.h>
+
+#include <lazyjson/variant.hpp>
+
+namespace json = lazyjson;
+
+/// @brief Создать класс одного типа и положить в него значение другого типа
+/// @tparam Ta первый тип
+/// @tparam Tb второй тип
+/// @param value значение
+template<typename Ta, typename Tb>
+void CreateVar(const Tb value)
+{
+    json::variant<Ta> var{value};
+}
+
+// Создание
+TEST(VariantUsage, CreationOk)
+{
+    ASSERT_NO_THROW((CreateVar<int, int>(int{})));
+    ASSERT_NO_THROW((CreateVar<size_t, size_t>(size_t{})));
+    ASSERT_NO_THROW((CreateVar<bool, bool>(bool{})));
+}
+
+// Создание с неверным типом
+TEST(VariantUsage, CreationBad)
+{
+    ASSERT_THROW((CreateVar<float, int>(int{})), std::bad_variant_access);
+    ASSERT_THROW((CreateVar<size_t, short>(short{})), std::bad_variant_access);
+    ASSERT_THROW((CreateVar<char, bool>(bool{})), std::bad_variant_access);
+}
+
+// Проверка установки значений
+TEST(VariantUsage, SetOkValues)
+{
+    json::variant<int, bool> var;
+
+    ASSERT_NO_THROW(var = int{});
+    ASSERT_NO_THROW(var = bool{});
+}
+
+// Проверка установки недоступных значений
+TEST(VariantUsage, SetBadValues)
+{
+    json::variant<int, bool> var;
+
+    ASSERT_THROW(var = float{}, std::bad_variant_access);
+    ASSERT_THROW(var = char{}, std::bad_variant_access);
+}
+
+// Проверка допустимых типов
+TEST(VariantUsage, CheckValidType)
+{
+    json::variant<int, bool, size_t> var;
+
+    ASSERT_TRUE(var.is_valid_type<int>());
+    ASSERT_TRUE(var.is_valid_type<bool>());
+    ASSERT_TRUE(var.is_valid_type<size_t>());
+}
+
+// Проверка допустимых типов
+TEST(VariantUsage, CheckInvalidType)
+{
+    json::variant<int, bool, size_t> var;
+
+    ASSERT_FALSE(var.is_valid_type<float>());
+    ASSERT_FALSE(var.is_valid_type<double>());
+    ASSERT_FALSE(var.is_valid_type<char>());
+}
+
+// Проверка типа
+TEST(VariantUsage, CheckType)
+{
+    json::variant<int, bool> var;
+
+    ASSERT_NO_THROW(var = int{});
+    ASSERT_TRUE(var.is_type<int>());
+    ASSERT_FALSE(var.is_type<bool>());
+
+    ASSERT_NO_THROW(var = bool{});
+    ASSERT_TRUE(var.is_type<bool>());
+    ASSERT_FALSE(var.is_type<int>());
+}
+
+// Проверка типа после очистки
+TEST(VariantUsage, CheckTypeAfterClear)
+{
+    json::variant<int, bool> var;
+
+    ASSERT_NO_THROW(var = int{});
+    ASSERT_TRUE(var.is_type<int>());
+
+    var.clear();
+
+    ASSERT_FALSE(var.is_type<int>());
+    ASSERT_FALSE(var.is_type<bool>());
+}
